3a20ca35f29617948df80f344227d7bc89f670d8
Yusril Ihsanul Alim <yusril.edmodo@outlook.co.id>
2022-07-21 00:07:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/main_compose_destinations/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.main_compose_destinations
+
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import com.example.main_compose_destinations.NavGraphs.root
+import com.example.main_compose_destinations.User
+import com.example.main_compose_destinations.destinations.PostScreenDestination
+import com.example.main_compose_destinations.destinations.ProfileScreenDestination
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import org.w3c.dom.Text
+import java.time.LocalDateTime
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DestinationsNavHost(navGraph = NavGraphs.root)
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Destination(start = true)
+@Composable
+fun LoginScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login Screen")
+        Button(onClick = {
+            navigator.navigate(
+                ProfileScreenDestination(
+                    User(
+                        name = "Ali",
+                        id = "userid",
+                        created = LocalDateTime.now()
+                    )
+                )
+            )
+        }) {
+            Text("Goto Profile Screen")
+            }
+    }
+}
+
+@Destination
+@Composable
+fun ProfileScreen(
+    navigator: DestinationsNavigator,
+    user: User
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Profile Screen $user", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navigator.navigate(PostScreenDestination())
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun PostScreen(
+    showOnlyPostsByUser: Boolean = false
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Post Screen, $showOnlyPostsByUser")
+    }
+}
